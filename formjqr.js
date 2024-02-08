576a055a7e0da6f5ead9dc0d0164bffea8fdd7b2
Ilyass jdidi <149004163+ilyassjd1@users.noreply.github.com>
2024-02-08 19:24:52 +0100

    Add files via upload

---
+++ formjqr.js
@@ -0,0 +1,107 @@
+// $(document).ready(function(){
+    // $("#prenom").focus();
+    // $("form").submit(function(){
+    //     var ok=true;
+    //     if($("#prenom").val()=="")
+    //     {
+    //         $("#prenom").next((".erreur").fadeIn().text("veuillez indiquer votr prenom "));
+    //         ok = false
+    //     }
+    //     else{ $("#prenom").next(".erreur").fadeOut();}
+
+    //     if($("#email").val()=="")
+    //     {
+    //         $("#email").next((".erreur").fadeIn().text("veuillez indiquer votr prenom "));
+    //         ok = false
+    //     }
+    //     else{ $("#email").next(".erreur").fadeOut();}
+
+    //     if($("#paye").val()=="")
+    //     {
+    //         $("#paye").next((".erreur").fadeIn().text("veuillez indiquer votr prenom "));
+    //         ok = false
+    //     }
+    //     else{ $("#paye").next(".erreur").fadeOut();}
+
+
+    
+
+//     $("#sub").click(function({
+//         pren_validation = $("#prenom").val()== "" ? false : true
+//         nom_validation = $("#nom").val()== "" ? false : true
+//         email_validation = $("#email").val()== "" ? false : true
+//         genre_validation = $("#input[name='genre'] ").is(":checked") ? true : false
+//         paye_validation = false
+//         $('#paye option').each(function(){
+//             if ($(this).is(":slelcted") && $("#paye").val()!==="")
+//                 paye_validation = true
+//         });
+//     }))
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+
+//     $('#first_form').submit(function(e) {
+//       e.preventDefault();
+//       var first_name = $('#first_name').val();
+//       var last_name = $('#last_name').val();
+//       var email = $('#email').val();
+//       var password = $('#password').val();
+  
+//       $(".error").remove();
+  
+//       if (first_name.length < 1) {
+//         $('#first_name').after('<span class="error">kteb smitek </span>');
+//       }
+//       if (last_name.length < 1) {
+//         $('#last_name').after('<span class="error">kteb knitek</span>');
+//       }
+//       if (email.length < 1) {
+//         $('#email').after('<span class="error">kteb email</span>');
+//       } else {
+//         var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/;
+//         var validEmail = regEx.test(email);
+//         if (!validEmail) {
+//           $('#email').after('<span class="error">kteb email s7i7</span>');
+//         }
+//       }
+//       if (password.length < 8) {
+//         $('#password').after('<span class="error">password khass ykun fih kter mn 8 caracteres</span>');
+//       }
+//       $('#paye option').each(function()
+//     });
+  
+//   });
+
+
+
+
+
+
+
+
+ 
